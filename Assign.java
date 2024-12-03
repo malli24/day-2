d7c61ca78975b41982e97e8907e93cd02ad434fd
malli24 <malleswari82006@gmail.com>
2024-12-03 12:35:49 +0530

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Assign
+{
+    public static void main(String[]args)
+    {
+        int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9); 
+        System.out.println(a*=2);
+    }
+}
