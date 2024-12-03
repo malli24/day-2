c8580b1f73836c321901906b4a5cf68b2f887491
malli24 <malleswari82006@gmail.com>
2024-12-03 15:54:45 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Shift
+{
+    public static void main(String[]args)
+    {
+        Scanner Sc=new Scanner(System.in);
+        int a=Sc.nextInt();
+        int b=Sc.nextInt();
+        System.out.println(a<<b);
+        System.out.println(a>>b);   
+    }
+}     
+        
+        
